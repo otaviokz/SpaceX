b5e9430fde9600ce1936f58f88c2b77569ac76bf
Otávio Zabaleta <otaviokz@gmail.coim>
2021-12-22 10:06:42 +0000

    First commit

---
+++ SpaceX/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  SpaceX
+//
+//  Created by Otávio Zabaleta on 22/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
